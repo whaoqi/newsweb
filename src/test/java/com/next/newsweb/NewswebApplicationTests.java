4c79d28afbc6c850956677a371cf639c08ad3f84
wen <wen93642674@gmail.com>
2020-03-06 20:26:15 +0800

    add index

---
+++ src/test/java/com/next/newsweb/NewswebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.next.newsweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewswebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
