11e10076684eda34b8001f8e27748fe051b6e2bc
wen <wen93642674@gmail.com>
2020-03-29 23:23:46 +0800

    add lombok

--- src/main/java/com/next/newsweb/mapper/UserMapper.java
+++ src/main/java/com/next/newsweb/mapper/UserMapper.java
@@ -8,7 +8,7 @@ import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
-    @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    @Insert("insert into user (name,account_id,avatar_url,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{avatarUrl}#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);//形参为类的时候会自动将类中属性带入到#{}里的值
 
     @Select("select * from user where token = #{token}")
--- src/main/resources/db/migration/V1__Create_user_table.sql
+++ src/main/resources/db/migration/V1__Create_user_table.sql
@@ -2,6 +2,7 @@ CREATE TABLE `user` (
   `id` int NOT NULL AUTO_INCREMENT,
   `account_id` varchar(100) DEFAULT NULL,
   `name` varchar(50) DEFAULT NULL,
+  `avatar_url` varchar(100) DEFAULT NULL,
   `token` char(36) DEFAULT NULL,
   `gmt_create` bigint DEFAULT NULL,
   `gmt_modified` bigint DEFAULT NULL,
