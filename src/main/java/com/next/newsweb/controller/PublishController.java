5dee157c351cc70c0a940451ab15ab4f2ce36fab
wen <wen93642674@gmail.com>
2020-04-10 16:54:09 +0800

    发布新闻添加标签选择

--- src/main/java/com/next/newsweb/controller/PublishController.java
+++ src/main/java/com/next/newsweb/controller/PublishController.java
@@ -1,10 +1,11 @@
 package com.next.newsweb.controller;
 
+import com.next.newsweb.cache.TagCache;
 import com.next.newsweb.dto.NewsDTO;
-import com.next.newsweb.mapper.NewsMapper;
 import com.next.newsweb.model.News;
 import com.next.newsweb.model.User;
 import com.next.newsweb.service.NewsService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -29,11 +30,14 @@ public class PublishController {
         model.addAttribute("content", news.getContent());
         model.addAttribute("tag", news.getTag());
         model.addAttribute("id", news.getId());
+        model.addAttribute("tags", TagCache.get());
         return "publish";
     }
 
+
     @GetMapping("/publish")
-    public String publish() {
+    public String publish(Model model) {
+        model.addAttribute("tags", TagCache.get());
         return "publish";
     }
 
@@ -47,6 +51,7 @@ public class PublishController {
         model.addAttribute("title", title);//接收后放在model里为了验证是否为空、回显
         model.addAttribute("content", content);
         model.addAttribute("tag", tag);
+        model.addAttribute("tags", TagCache.get());
 
         if (title == null || title == "") {
             model.addAttribute("error", "新闻标题不能为空");
@@ -60,6 +65,13 @@ public class PublishController {
             model.addAttribute("error", "标签不能为空");
             return "publish";
         }
+
+        String invalid = TagCache.filterInvalid(tag);
+        if (StringUtils.isNotBlank(invalid)) {
+            model.addAttribute("error", "输入非法标签:" + invalid);
+            return "publish";
+        }
+
         User user = (User) request.getSession().getAttribute("user");//获取session里的user并且强转达到下条的user为空时跳转首页
         if (user == null) {
             model.addAttribute("error", "用户未登录");
---
+++ src/main/java/com/next/newsweb/dto/TagDTO.java
@@ -0,0 +1,11 @@
+package com.next.newsweb.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class TagDTO {
+    private String categoryName;
+    private List<String> tags;
+}
